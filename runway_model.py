699bf0954323d9bab24e4b6fe03971b3e272f767
ak9250 <ahsen.khaliq@gmail.com>
2021-01-12 12:55:47 -0500

    try zero-shot

--- runway_model.py
+++ runway_model.py
@@ -1,24 +1,37 @@
-import torch
+import os
 import clip
-from PIL import Image
+import torch
+from torchvision.datasets import CIFAR100
 import runway
 
+# Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, transform = clip.load("ViT-B/32", device=device)
+model, preprocess = clip.load('ViT-B/32', device)
+
+# Download the dataset
+cifar100 = CIFAR100(root=os.path.expanduser("~/.cache"), download=True, train=False)
+
 
 @runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated')}, outputs={'text': runway.text})
 def translate(learn, inputs):
-    image = transform(inputs['source_imgs']).unsqueeze(0).to(device)
-    text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+    image_input = preprocess(inputs['source_imgs']).unsqueeze(0).to(device)
+    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in cifar100.classes]).to(device)
 
+    # Calculate features
     with torch.no_grad():
-        image_features = model.encode_image(image)
-        text_features = model.encode_text(text)
-        
-        logits_per_image, logits_per_text = model(image, text)
-        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-        
-    return probs
+        image_features = model.encode_image(image_input)
+        text_features = model.encode_text(text_inputs)
+
+    # Pick the top 5 most similar labels for the image
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    values, indices = similarity[0].topk(5)
+
+    # Print the result
+    for value, index in zip(values, indices):
+        text = f"{cifar100.classes[index]:>16s}: {100 * value.item():.2f}%"
+    return text
 
 if __name__ == '__main__':
     runway.run(port=8889)
