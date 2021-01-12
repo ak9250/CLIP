26200a7b74c700d8e93fb6e2ac3da440a63842b1
ak9250 <ahsen.khaliq@gmail.com>
2021-01-12 11:37:39 -0500

    install setup

--- runway_model.py
+++ runway_model.py
@@ -1,44 +1,24 @@
-import os
-import clip
 import torch
-from torchvision.datasets import CIFAR100
-import runway
-import numpy as np
-import argparse
+import clip
+from PIL import Image
 
-# Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load('ViT-B/32', device)
-
-# Download the dataset
-cifar100 = CIFAR100(root=os.path.expanduser("~/.cache"), download=True, train=False)
-
-
+model, transform = clip.load("ViT-B/32", device=device)
 
 
 @runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),}, outputs={'text': runway.text(description='output image containing the translated result')})
 def translate(learn, inputs):
-    
-    # Prepare the inputs
-    image_input = preprocess(inputs['source_imgs']).unsqueeze(0).to(device)
-    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in cifar100.classes]).to(device)
+    image = transform(inputs['source_imgs']).unsqueeze(0).to(device)
+    text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 
-    # Calculate features
     with torch.no_grad():
-        image_features = model.encode_image(image_input)
-        text_features = model.encode_text(text_inputs)
-
-    # Pick the top 5 most similar labels for the image
-    image_features /= image_features.norm(dim=-1, keepdim=True)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
-    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-    values, indices = similarity[0].topk(5)
-
-    # Print the result
-    for value, index in zip(values, indices):
-        text = f"{cifar100.classes[index]:>16s}: {100 * value.item():.2f}%"
-
-    return text
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(text)
+        
+        logits_per_image, logits_per_text = model(image, text)
+        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+        
+    return probs
 
 
 if __name__ == '__main__':
